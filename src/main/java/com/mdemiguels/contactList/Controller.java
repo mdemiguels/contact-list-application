a9d0c5d2cf2b0434112b66699e62198f87491418
Miguel Sánchez Ramírez <miguelschz1997@gmail.com>
2024-07-07 18:32:32 +0200

    Finalizada UI con SceneBuilder. Creado Controlador para dar funcionalidad a los elementos de la interfaz

--- src/main/java/com/mdemiguels/contactList/Controller.java
+++ src/main/java/com/mdemiguels/contactList/Controller.java
@@ -1,7 +1,44 @@
 package com.mdemiguels.contactList;
 
+import com.mdemiguels.contactList.data.ContactRepository;
+import com.mdemiguels.contactList.model.Contact;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Component
-public class Controller {
+public class Controller implements Initializable {
+
+    private final ContactRepository contactRepository;
+
+    public Controller(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    @FXML
+    AnchorPane anchorPane;
+    @FXML
+    ListView<Contact> listView;
+    @FXML
+    TextField txtId;
+    @FXML
+    TextField txtName;
+    @FXML
+    TextField txtEmail;
+    @FXML
+    TextField txtPhone;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
+    }
+
 }
