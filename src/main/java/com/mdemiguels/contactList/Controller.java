8754035d85304a12d5a12645babe543710c83f4d
Miguel Sánchez Ramírez <miguelschz1997@gmail.com>
2024-07-16 19:39:31 +0200

    Finalizadas funcionalidades editar y eliminar Contactos

--- src/main/java/com/mdemiguels/contactList/Controller.java
+++ src/main/java/com/mdemiguels/contactList/Controller.java
@@ -32,13 +32,21 @@ public class Controller implements Initializable {
     @FXML
     TextField txtId, txtName, txtEmail, txtPhone;
     @FXML
-    Button btnAdd, btnUpdate, btnDelete;
+    Button btnSave, btnUpdate, btnDelete;
     @FXML
     Label lblContacts, lblWarning;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         list();
+        listView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                txtId.setText(String.valueOf(newValue.getId()));
+                txtName.setText(newValue.getName());
+                txtEmail.setText(newValue.getEmail());
+                txtPhone.setText(newValue.getPhone());
+            }
+        });
     }
 
     public void list() {
@@ -52,10 +60,7 @@ public class Controller implements Initializable {
     @FXML
     public void add() {
 
-        if(txtName.getText().isEmpty()) {
-            lblWarning.setText("Name is required");
-            return;
-        }
+        if (checkValidFields()) return;
 
         Contact contact = new Contact();
         contact.setName(txtName.getText());
@@ -74,6 +79,65 @@ public class Controller implements Initializable {
         txtEmail.clear();
         txtPhone.clear();
         lblWarning.setText("");
+
+        btnSave.setDisable(false);
+        btnUpdate.setDisable(true);
+        btnDelete.setDisable(true);
+    }
+
+    @FXML
+    public void update() {
+
+        if (checkValidFields()) return;
+
+        Contact contact = new Contact();
+        contact.setId(Long.parseLong(txtId.getText()));
+        contact.setName(txtName.getText());
+        contact.setEmail(txtEmail.getText());
+        contact.setPhone(txtPhone.getText());
+        contactRepository.save(contact);
+
+        list();
+        clear();
+    }
+
+    @FXML
+    public void contactSelected() {
+        btnSave.setDisable(true);
+        btnUpdate.setDisable(false);
+        btnDelete.setDisable(false);
+    }
+
+    @FXML
+    public void delete() {
+
+        if(txtId.getText().isEmpty()) {
+            lblWarning.setText("Select a contact to delete");
+            return;
+        }
+
+        contactRepository.deleteById(Long.parseLong(txtId.getText()));
+
+        list();
+        clear();
+    }
+
+    private boolean checkValidFields() {
+        if(txtName.getText().isEmpty()) {
+            lblWarning.setText("Name is required");
+            return true;
+        }
+
+        if (txtPhone.getLength() != 9) {
+            lblWarning.setText("Phone must have 9 digits");
+            return true;
+        }
+
+        if (!txtEmail.getText().isEmpty() && !txtEmail.getText().matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+            lblWarning.setText("Invalid email");
+            return true;
+        }
+        return false;
     }
 
 }
